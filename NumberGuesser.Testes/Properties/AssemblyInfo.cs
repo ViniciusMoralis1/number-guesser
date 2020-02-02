53740a6f8bb5bbf88be2d621b71e117675c8cb10
Vinicius Moralis <vinicius_moralis@hotmail.com>
2020-02-02 17:16:19 -0300

    adicionando tentativas e pontuação

---
+++ NumberGuesser.Testes/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NumberGuesser.Testes")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NumberGuesser.Testes")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("568509c8-0377-43b6-9329-f1d3455c5e61")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- NumberGuesser/Program.cs
+++ NumberGuesser/Program.cs
@@ -1,33 +1,27 @@
 ﻿using System;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
 
 namespace NumberGuesser
 {
-    class Chamadas
+    //public partial class Program : Form
+    public class Program
     {
-        public static void GetAppName()
-        {
-            string appName = "Number Guesser";
-            string appVersion = "1.0.0";
-            string appAuthor = "Vinicius Moralis";
 
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine($"{appName}: Version {appVersion} Created by {appAuthor}");
-            Console.ResetColor();
-        }
+        //public Program()
+        //{
+        //InitializeComponent();
+        //}
 
-        public static void PrintColorAndMessage(ConsoleColor color, string message)
+        static void Main(string[] args)
         {
-            Console.ForegroundColor = color;
-            Console.WriteLine(message);
-            Console.ResetColor();
-        }
-    }
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Program());
 
-    class Program //classe principal
-    {
-        static void Main(string[] args) //método de entrada
-        {
             Chamadas.GetAppName();
+
+            int score = 0;
 
             Console.WriteLine("What's your name?");
             string userName = Console.ReadLine();
@@ -39,10 +33,11 @@ namespace NumberGuesser
                 Random random = new Random();
                 int correctNumber = random.Next(1, 10);
 
-                Console.WriteLine("Guess a number between 1 and 10");
+                Console.WriteLine("You have 3 tries to guess a number between 1 and 10");
                 int guess = 0;
+                int chances = 3;
 
-                while (guess != correctNumber)
+                while (guess != correctNumber && chances > 0)
                 {
                     string number = Console.ReadLine();
                     if (!int.TryParse(number, out guess))
@@ -55,10 +50,33 @@ namespace NumberGuesser
                     if (guess != correctNumber)
                     {
                         Chamadas.PrintColorAndMessage(ConsoleColor.Red, "Wrong Number, try again!");
+                        chances = chances - 1;
+                    }
+                    if (chances == 0)
+                    {
+                        Chamadas.PrintColorAndMessage(ConsoleColor.Red, "You are of chances!!!");
                     }
                 }
 
-                Chamadas.PrintColorAndMessage(ConsoleColor.Yellow, "You are correct!!!");
+                if(guess == correctNumber)
+                {
+                    Chamadas.PrintColorAndMessage(ConsoleColor.Yellow, "You are correct!!!");
+                    if(chances == 3)
+                    {
+                        score = score + 20;
+                        Console.WriteLine($"Your new score is {score}");
+                    }
+                    if (chances == 2)
+                    {
+                        score = score + 10;
+                        Console.WriteLine($"Your new score is {score}");
+                    }
+                    if(chances == 1)
+                    {
+                        score = score + 5;
+                        Console.WriteLine($"Your new score is {score}");
+                    }
+                }
 
                 Chamadas.PrintColorAndMessage(ConsoleColor.Green, "Play again? Y/N");
                 string answer = Console.ReadLine().ToUpper();
@@ -71,6 +89,27 @@ namespace NumberGuesser
                 {
                     return;
                 }
+            }
+        }
+
+        public class Chamadas
+        {
+            public static void GetAppName()
+            {
+                string appName = "Number Guesser";
+                string appVersion = "1.0.0";
+                string appAuthor = "Vinicius Moralis";
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"{appName}: Version {appVersion} Created by {appAuthor}");
+                Console.ResetColor();
+            }
+
+            public static void PrintColorAndMessage(ConsoleColor color, string message)
+            {
+                Console.ForegroundColor = color;
+                Console.WriteLine(message);
+                Console.ResetColor();
             }
         }
     }
